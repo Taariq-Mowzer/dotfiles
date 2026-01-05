2c61b2acc935aab45bcab69a87894017058c43f8
Taariq Mowzer <taariq@thinkst.com>
2026-01-05 09:40:14 +0200

    Change windows with <leader>hjkl
    There is a conflict with <leader>ls opening the buffer search, but this
    is fine for now

--- dot_config/nvim/lua/taariq/remap.lua
+++ dot_config/nvim/lua/taariq/remap.lua
@@ -4,10 +4,10 @@ vim.keymap.set("n", "<leader>=", vim.cmd.vsplit)
 vim.keymap.set("n", "<leader>-", vim.cmd.split)
 
 -- Move between splits with CTRL + h/j/k/l
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<leader>h", "<C-w>h")
+vim.keymap.set("n", "<leader>j", "<C-w>j")
+vim.keymap.set("n", "<leader>k", "<C-w>k")
+vim.keymap.set("n", "<leader>l", "<C-w>l")
 -- Move to next split with <leader> + o
 vim.keymap.set("n", "<leader>o", "<C-w>w")
 
