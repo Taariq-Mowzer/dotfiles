d7f6d4e2ef804027fd11c1d272da8a3b2eb1c531
Taariq Mowzer <taariq@thinkst.com>
2026-02-23 12:06:21 +0200

    Use <leader>mm to open up local marks

--- dot_config/nvim/plugin/telescope.lua
+++ dot_config/nvim/plugin/telescope.lua
@@ -1,6 +1,11 @@
 local builtin = require('telescope.builtin')
 local action_state = require('telescope.actions.state')
 local actions = require('telescope.actions')
+local utils = require('telescope.utils')
+local finders = require('telescope.finders')
+local pickers = require('telescope.pickers')
+local make_entry = require('telescope.make_entry')
+local conf = require("telescope.config").values
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 
@@ -54,6 +59,60 @@ vim.api.nvim_create_user_command('LS',  function()
 	local output_reversed = table.concat(lines, "\n")
 	vim.notify(output_reversed)
 end, {})
-	
+
 vim.keymap.set('n', '<leader>ls', buffer_searcher, {})
 vim.keymap.set('n', '<leader>bl', ':LS<CR>:b ', { noremap = true })
+
+local function local_marks()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local local_marks = {
+    items = vim.fn.getmarklist(bufnr),
+    name_func = function(_, line)
+      return vim.api.nvim_buf_get_lines(bufnr, line - 1, line, false)[1]
+    end,
+  }
+  local marks_table = {}
+  local marks_others = {}
+  local bufname = vim.api.nvim_buf_get_name(bufnr)
+  for _, v in ipairs(local_marks.items) do
+    -- strip the first single quote character
+    local mark = string.sub(v.mark, 2, 3)
+    local _, lnum, col, _ = unpack(v.pos)
+    local name = local_marks.name_func(mark, lnum)
+    -- same format to :marks command
+    local line = string.format("%s %6d %4d %s", mark, lnum, col - 1, name)
+    local row = {
+      line = line,
+      lnum = lnum,
+      col = col,
+      filename = utils.path_expand(v.file or bufname),
+    }
+    -- non alphanumeric marks goes to last
+	print(mark)
+    if mark:match "%w" then
+      table.insert(marks_table, row)
+    end
+  end
+  marks_table = vim.fn.extend(marks_table, marks_others)
+  pickers
+    .new({}, {
+      prompt_title = "Marks",
+      finder = finders.new_table {
+        results = marks_table,
+        entry_maker = make_entry.gen_from_marks({}),
+      },
+      previewer = conf.grep_previewer({}),
+      sorter = conf.generic_sorter({}),
+	  layout_strategy = "horizontal",
+		layout_config = {
+		  preview_width = 0.9, -- preview takes 40%, results take the rest
+		  -- width = 0.8,
+		  -- height = 0.6,
+		},
+      push_cursor_on_edit = true,
+      push_tagstack_on_edit = true,
+    })
+    :find()
+end
+
+vim.keymap.set('n', '<leader>mm', local_marks, { desc = "Telescope: local marks" })
