ef13a5777ae69c1e38566593bcd06648c651b9d1
Taariq Mowzer <taariq@thinkst.com>
2026-06-01 11:24:29 +0200

    Better looking folds

--- dot_config/nvim/lua/taariq/theme.lua
+++ dot_config/nvim/lua/taariq/theme.lua
@@ -9,3 +9,34 @@ vim.opt.relativenumber = true
 vim.opt.signcolumn = 'yes'
 
 vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'NONE' })
+
+function _G.NeatFoldText()
+  local line = " " .. vim.fn.substitute(
+    vim.fn.getline(vim.v.foldstart),
+    [[^\s*"\?\s*\|\s*"\?\s*{{\d*\s*]],
+    "",
+    "g"
+  ) .. " "
+
+  local lines_count = vim.v.foldend - vim.v.foldstart + 1
+  local lines_count_text = "| " .. string.format("%10s", lines_count .. " lines") .. " |"
+
+  local fillchars = vim.opt.fillchars:get()
+  local foldchar = fillchars.fold or " "
+
+  local foldtextstart = string.sub(
+    "+" .. string.rep(foldchar, vim.v.foldlevel * 2) .. line,
+    1,
+    math.floor((vim.fn.winwidth(0) * 2) / 3)
+  )
+
+  local foldtextend = lines_count_text .. string.rep(foldchar, 8)
+
+  local foldtextlength = vim.fn.strdisplaywidth(foldtextstart .. foldtextend) + vim.wo.foldcolumn
+  local padding = math.max(vim.fn.winwidth(0) - foldtextlength, 0)
+
+  return foldtextstart .. string.rep(" ", padding) .. foldtextend
+end
+
+vim.opt.foldtext = "v:lua.NeatFoldText()"
+vim.opt.fillchars:append({diff = " "})
